dd4b6cb94557cf8765b76058f81d139552d409c0
Matt Hayashida <matt.hayashida@appcues.com>
2024-05-09 13:03:58 -0400

    ♻️ Allow for empty plugin props

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -3,7 +3,7 @@ import { ConfigPlugin } from 'expo/config-plugins';
 import { ConfigProps } from './types';
 import { withIosAppcuesRichPush } from './withIosAppcuesRichPush';
 
-const withAppcuesPush: ConfigPlugin<ConfigProps> = (config, props) => {
+const withAppcuesPush: ConfigPlugin<ConfigProps> = (config, props = {}) => {
   config = withIosAppcuesRichPush(config, props);
 
   return config;
